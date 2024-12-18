0d9d53c423167de39b17f9793d5ce0e60cc35462
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2024-12-18 16:23:02 +1000

    Add files via upload

---
+++ admin/includes/languages/english/extra_definitions/lang.recover_cart.php
@@ -0,0 +1,8 @@
+<?php
+$define = [ 
+   'BOX_REPORTS_RECOVER_CART_SALES' => 'Recovered Sales Results',
+   'BOX_TOOLS_RECOVER_CART' => 'Abandoned Carts',
+   'BOX_CONFIG_RECOVER_CART_SALES' => 'Configure RCS',
+];
+return $define; 
+
